2c2c8527924a551f41745ae0b5bd255a3d7c56de
challengerZeng <664903471@qq.com>
2018-06-03 01:06:14 +0800

    全文搜索

---
+++ Elasticsearch/src/main/java/com/bsb/repository/EsBlogRepository.java
@@ -0,0 +1,15 @@
+package com.bsb.repository;
+
+import com.bsb.domain.EsBlog;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+
+/**
+ * Es资源库接口
+ */
+public interface EsBlogRepository extends ElasticsearchRepository<EsBlog,String> {
+
+    Page<EsBlog> findDistinctEsBlogByTitleContainingOrSummaryContainingOrContentContaining(
+            String title, String summary, String content, Pageable pageable);
+}
