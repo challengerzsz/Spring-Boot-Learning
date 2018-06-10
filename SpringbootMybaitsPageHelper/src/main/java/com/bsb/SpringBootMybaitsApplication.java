3379dcee1a6657443618443d3827ec65211925ea
challengerZeng <664903471@qq.com>
2018-06-10 20:27:24 +0800

    lombok pageHelper整合

---
+++ SpringbootMybaitsPageHelper/src/main/java/com/bsb/SpringBootMybaitsApplication.java
@@ -0,0 +1,21 @@
+package com.bsb;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@MapperScan("com.bsb.mapper")
+@SpringBootApplication
+public class SpringBootMybaitsApplication extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMybaitsApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(SpringBootMybaitsApplication.class);
+    }
+}
